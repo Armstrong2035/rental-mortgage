5c8378abb0a7167807ae9ea348f9e668250cfdc3
Armstrong Olusoji <armstrongolusoji9@gmail.com>
2025-08-20 20:21:33 +0400

    Fixed lint issues, and changed the root route

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -1,30 +1,9 @@
-import ScrollingAppBar from "../components/landing/appBar/ScrollingAppBar";
-import Body from "../components/landing/body/Body";
-import Head from "next/head";
-import { hero } from "./utils/siteConfig";
+import Onboarding from "../components/onboarding/Onboarding";
 
-export const dynamic = "force-static";
-
-export default function Home() {
+export default function Page() {
   return (
-    <>
-      <Head>
-        <link
-          rel="preload"
-          href={hero.heroImage}
-          as="image"
-          type="image/webp"
-        />
-      </Head>
-      <div>
-        <ScrollingAppBar />
-        <Body
-          // images={gallery.images}
-          heroImage={hero.heroImage}
-          // masterPlan={location.masterPlan}
-          // floorPlanImages={floorPlans.images}
-        />
-      </div>
-    </>
+    <div style={{ height: "100vh" }}>
+      <Onboarding />
+    </div>
   );
 }
